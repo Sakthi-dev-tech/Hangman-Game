6589f983e0de5cc048172390d0625b14fda865fd
Sakthi-dev-tech <mssakthieesvar@gmail.com>
2024-03-19 14:19:53 +0800

    Adding in of the CSS file and Hangman logo
    Webpage not showing the CSS formatting

--- views.py
+++ views.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 
 views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html") 
