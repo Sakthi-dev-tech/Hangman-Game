aaee76b3b251b00d8bd940355f220d62edf70d57
Sakthi-dev-tech <mssakthieesvar@gmail.com>
2024-03-28 19:38:46 +0800

    Added in a requirements.txt to ensure all libraries are installed when running this web app

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from views import views
 import jinja2
 
+#RUN pip install -r requirements.txt in your terminal first before running
+
 app = Flask(__name__)
 app.register_blueprint(views, url_prefix="/")
 
